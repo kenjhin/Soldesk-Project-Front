8ff01e2875ce921a0407b37150b9645625fd1c9b
SangJin Lee <9607sj@gmail.com>
2024-02-15 11:34:23 +0900

    페이지 라우터 처리 및 RB 라이브러리 설치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+/* eslint-disable */ 
+import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, useNavigate, Outlet } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
+  let [제목, 제목변경] = useState(['기무링', '기무링딩동']);
+  let [따봉, 따봉변경] = useState(0);
+  let navigate = useNavigate();
+
+
   return (
+    <div>
     
-    <div> Soldesk Team Project1 </div>
+
+
+      <Routes>
+        <Route path='/' element={
+            <>
+            <div className='screen1'>
+              <button className='homeBtn' onClick={()=>{navigate('/')}}>홈</button>
+              <button className='homeBtn' onClick={()=>{navigate('/Login')}} >로그인</button>
+              </div>
+          <div className='screen2'>
+              
+            </div>
+            </>
+
+        }/>
+        <Route path='/Login' element={<Login/>}/>
+        
+      </Routes>
+    </div>
   );
 }
 
