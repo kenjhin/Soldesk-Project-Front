6b5cae0e41be3062ad0e4bdc1f67cbb6447ff7d3
blackpendereum <blackpendereum@naver.com>
2024-02-15 09:27:13 +0900

    UI Base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+/* eslint-disable */ 
+import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [제목, 제목변경] = useState(['기무링', '기무링딩동']);
+  let [따봉, 따봉변경] = useState(0);
   return (
-    
-    <div> Soldesk Team Project1 </div>
+    <div>
+      <div className='screen1'>
+        <button className='homeBtn'>홈</button>
+      </div>
+      <div className='screen2'>
+
+      </div>
+    </div>
   );
 }
 
