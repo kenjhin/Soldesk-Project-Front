3975820584c5330dba34df19229cafc0a10363e7
blackpendereum <blackpendereum@naver.com>
2024-02-19 10:26:24 +0900

    ui_0218

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 /* eslint-disable */ 
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './styles/App.css';
+import storeIco from './assets/img/store_ico.png'
+import myInfoIco from './assets/img/myInfo_ico.png'
+import inventoryIco from './assets/img/inventory_ico.png'
+import hamster from './assets/img/hamster.jpg'
+import nbg from './assets/font/NanumBarunGothic.ttf'
+import nbgB from './assets/font/NanumBarunGothicBold.ttf'
+import nbgL from './assets/font/NanumBarunGothicLight.ttf'
+import nbgUL from './assets/font/NanumBarunGothicUltraLight.ttf'
 function App() {
-  let [제목, 제목변경] = useState(['기무링', '기무링딩동']);
-  let [따봉, 따봉변경] = useState(0);
-  return (
-    <div>
-      <div className='screen1'>
-        <button className='homeBtn'>홈</button>
-      </div>
-      <div className='screen2'>
 
+  return (
+    <>
+      <header className='header'>
+        <div className='headerBtnBox'>
+          <div className='headerBox1'>
+            <button className='noticeBtn'>!</button>
+            <button className='homeBtn'>홈</button>
+            <img className='myInfoBtn' src={myInfoIco} alt=''/>
+            <img className='inventoryBtn' src={inventoryIco} alt=''/>
+            <img className='storeBtn' src={storeIco} alt=''/>
+          </div>
+          <div className='headerBox2'>
+            <img className='userIcon' src={hamster} alt=''/>
+          </div>
+        </div>
+      </header>
+          
+      <div className='messenger'>
+        <button className='messengerText'>커뮤니티</button>
+        <button className='messengerBtn'>ㅇ</button>
+        <button className='messengerBtn'>ㅇ</button>
+        <button className='messengerBtn'>ㅇ</button>
+        <button className='messengerBtn'>ㅇ</button>
       </div>
-    </div>
+
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
 
